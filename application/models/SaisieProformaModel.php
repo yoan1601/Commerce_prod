1b07d8282d55d3761ec5a2fb2ee6836dbe527977
Eriq606 <erohweltall@gmail.com>
2023-11-17 21:10:50 +0300

    saisieproforma

--- application/models/SaisieProformaModel.php
+++ application/models/SaisieProformaModel.php
@@ -14,4 +14,27 @@ class SaisieProformaModel extends CI_Model {
         $query=$query->result();
         return $query;
     }
+    public function insertProforma($fournisseur, $delai, $details){
+        $query="insert into proformas(id_fournisseur_proforma, delai_livraison_proforma) values(%s, '%s')";
+        $query=sprintf($query, $fournisseur, $delai);
+        $this->db->query($query);
+        $lastId=$this->getLastIdProforma();
+        foreach($details as $detail){
+            $this->insertDetailProforma($detail, $lastId);
+        }
+    }
+    public function getLastIdProforma(){
+        $query="select max(id_proforma) as last_id from proformas";
+        $query=$this->db->query($query);
+        $query=$query->result();
+        if(count($query)>0){
+            return $query[0]->last_id;
+        }
+        return $query;
+    }
+    public function insertDetailProforma($detail, $proforma){
+        $query="insert into detail_proformas(id_article_detail_proforma, quantite_detail_proforma, puht_detail_proforma, tva_detail_proforma, ttc_detail_proforma, id_proforma_detail_proforma) values(%s, %s, %s, %s, %s, %s)";
+        $query=sprintf($query, $detail["article"], $detail["qte"], $detail["puht"], $detail["tva"], $detail["ttc"], $proforma);
+        $this->db->query($query);
+    }
 }
